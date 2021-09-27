e57c8872a2c493824a71da34a8b0cdd879c54804
nisadevapp <47986422+nisadevapp@users.noreply.github.com>
2021-09-27 08:23:50 +0200

    Versión 1 - Contador App
    Aplicación finalizada del Curso Vue 3

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            title: "Contador App -Vue",
+            count: 0
+        }
+    },
+    methods: {
+        modCount(instruction = "add", limit = 1) {
+            if (instruction === 'dis')
+                this.count -= limit;
+            else
+                this.count += limit;
+        },
+        reset() {
+            this.count = 0;
+        }
+    }
+})
